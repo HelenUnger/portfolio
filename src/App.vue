8b04c903413ae40f8220c2da0cde562198b8e7b2
Helen Unger <helen-unger@hotmail.com>
2020-05-09 23:31:47 -0400

    vue set up using vue cli

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Header />
+    <Skills />
+    <About />
+  </div>
+</template>
+
+<script>
+import Header from './components/header.vue'
+import Skills from './components/skills.vue'
+import About from './components/about.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Skills,
+    About
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/about.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        about
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            message: 'message',
+        };
+    }
+}
+</script>
